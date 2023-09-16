9dc661fadd058ad4e98384b5caf15b7f80ef101e
Ashwin Das <ashwindas1113@gmail.com>
2023-09-16 09:45:30 -0500

    Final Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,12 +1,26 @@
 package com.driver;
 
 public class Main {
-    public Main() {
-    }
-
     public static void main(String[] args) {
         B object = new B();
-        System.out.print(object.meth());
+        object.meth();
+
+        B object2 = new B();
+        object2.meth();
+    }
+
+    public static class A {
+        String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A {
+        // Task 4: Override the meth method in class B.
+        @Override
+        String meth() {
+            return "Method is overridden in Extended class B";
+        }
     }
 }
 
