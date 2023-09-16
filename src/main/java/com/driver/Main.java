6089169a42dd7a22d00866da2b653e7f3c5ad0a4
Ashwin Das <ashwindas1113@gmail.com>
2023-09-16 09:29:04 -0500

    ??

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,9 @@
 package com.driver;
 
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
         B object = new B();
         System.out.print(object.meth());
