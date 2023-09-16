84e97db50193ef086bee7079c4673d7ca12c0a9e
Ashwin Das <ashwindas1113@gmail.com>
2023-09-16 09:15:32 -0500

    Updated

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,6 +1,6 @@
 package com.driver;
 
-public class B extends A{
+class B extends A{
          @Override
         public String meth(){
             return "Method is overridden in Extendend class B";
